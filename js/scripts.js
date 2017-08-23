809523629c2d0e6401bea3e783035760ad584e3c
KamiAmiga <camille.gicquel@corp.ovh.com>
2017-08-23 15:02:05 +0200

    :lipstick: style(global): minor styles fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 var menu = document.getElementById('footer-nav');
+var menuArrow = document.getElementsByClassName('footer-nav__arrow');
+var menuLogo = document.getElementsByClassName('footer-nav__ux-logo');
 var navBtns = document.getElementsByClassName('home-nav__link');
 var sections = document.getElementsByClassName('section');
 var sectionsContent = document.getElementsByClassName('section__content');
@@ -13,6 +15,8 @@ var btnContribute = document.getElementById('btn-contribute');
 
 function backHome() {
     menu.classList.remove('footer-nav--active', 'footer-nav--about-us', 'footer-nav--works', 'footer-nav--contribute');
+    menuArrow[0].classList.remove('footer-nav__arrow--about-us', 'footer-nav__arrow--works', 'footer-nav__arrow--contribute');
+    menuLogo[0].classList.remove('footer-nav__ux-logo--about-us', 'footer-nav__ux-logo--works', 'footer-nav__ux-logo--contribute');
 
     for (i = 0; i < sections.length; i++) {
         sections[i].classList.remove('section--active');
@@ -26,6 +30,8 @@ function backHome() {
 function teamAppear() {
     sectionTeam.classList.add('section--active');
     menu.classList.add('footer-nav--active', 'footer-nav--about-us');
+    menuArrow[0].classList.add('footer-nav__arrow--about-us');
+    menuLogo[0].classList.add('footer-nav__ux-logo--about-us');
     var sectionContent = sectionTeam.getElementsByClassName('section__content');
     sectionContent[0].classList.add('section__content--active');
 }
@@ -33,6 +39,8 @@ function teamAppear() {
 function worksAppear() {
     sectionWorks.classList.add('section--active');
     menu.classList.add('footer-nav--active', 'footer-nav--works');
+    menuArrow[0].classList.add('footer-nav__arrow--works');
+    menuLogo[0].classList.add('footer-nav__ux-logo--works');
     var sectionContent = sectionWorks.getElementsByClassName('section__content');
     sectionContent[0].classList.add('section__content--active');
 }
@@ -40,6 +48,8 @@ function worksAppear() {
 function contributeAppear() {
     sectionContribute.classList.add('section--active');
     menu.classList.add('footer-nav--active', 'footer-nav--contribute');
+    menuArrow[0].classList.add('footer-nav__arrow--contribute');
+    menuLogo[0].classList.add('footer-nav__ux-logo--contribute');
     var sectionContent = sectionContribute.getElementsByClassName('section__content');
     sectionContent[0].classList.add('section__content--active');
 }
