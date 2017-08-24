863b4782e55375ab6d049adac60f4f1f16f00ac1
Julien DONQUE <julien.donque@corp.ovh.com>
2017-08-24 12:58:04 +0200

    :tractor: refactor(js): refactor to es5 :'(

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,36 @@
-const menu = document.getElementById('footer-nav');
-const menuArrow = document.querySelector('.footer-nav__arrow');
-const menuLogo = document.querySelector('.footer-nav__ux-logo');
+var menu = document.getElementById("footer-nav");
+var menuArrow = document.querySelector(".footer-nav__arrow");
+var menuLogo = document.querySelector(".footer-nav__ux-logo");
 
-const sections = document.getElementsByClassName('section');
-const sectionsContent = document.getElementsByClassName('section__content');
-const sectionIds = ['about-us', 'works', 'contribute'];
+var sections = document.getElementsByClassName("section");
+var sectionsContent = document.getElementsByClassName("section__content");
+var sectionIds = ["about-us", "works", "contribute"];
 
-menu.addEventListener('click', () => {
-    menu.classList.remove('footer-nav--active');
-    sectionIds.forEach((sectionId) => {
-        menu.classList.remove(`footer-nav--${sectionId}`);
-        menuArrow.classList.remove(`footer-nav__arrow--${sectionId}`);
-        menuLogo.classList.remove(`footer-nav__ux-logo--${sectionId}`);
+menu.addEventListener("click", function () {
+    menu.classList.remove("footer-nav--active");
+    sectionIds.forEach(function (sectionId) {
+        menu.classList.remove("footer-nav--" + sectionId);
+        menuArrow.classList.remove("footer-nav__arrow--" + sectionId);
+        menuLogo.classList.remove("footer-nav__ux-logo--" + sectionId);
     });
 
-    for (let i = 0; i < sections.length; i++) {
-        sections[i].classList.remove('section--active');
+    for (var i = 0; i < sections.length; i++) {
+        sections[i].classList.remove("section--active");
     }
 
-    for (let i = 0; i < sectionsContent.length; i++) {
-        sectionsContent[i].classList.remove('section__content--active');
+    for (var i = 0; i < sectionsContent.length; i++) {
+        sectionsContent[i].classList.remove("section__content--active");
     }
 });
 
-sectionIds.forEach((sectionId) =>
-    document.getElementById(`btn-${sectionId}`).addEventListener('click', () => {
-        const section = document.getElementById(sectionId);
-        const sectionContent = document.querySelector(`#${sectionId} .section__content`);
-        menu.classList.add('footer-nav--active', `footer-nav--${sectionId}`);
-        menuArrow.classList.add(`footer-nav__arrow--${sectionId}`);
-        menuLogo.classList.add(`footer-nav__ux-logo--${sectionId}`);
-        section.classList.add('section--active');
-        sectionContent.classList.add('section__content--active');
-}));
+sectionIds.forEach(function (sectionId) {
+    document.getElementById("btn-" + sectionId).addEventListener("click", function () {
+        var section = document.getElementById(sectionId);
+        var sectionContent = document.querySelector("#" + sectionId + " .section__content");
+        menu.classList.add("footer-nav--active", "footer-nav--" + sectionId);
+        menuArrow.classList.add("footer-nav__arrow--" + sectionId);
+        menuLogo.classList.add("footer-nav__ux-logo--" + sectionId);
+        section.classList.add("section--active");
+        sectionContent.classList.add("section__content--active");
+    });
+});
