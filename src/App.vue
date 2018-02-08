d1cb8b47035da6e14738f6d4f43495edd507f626
Camille Gicquel <camille.gicquel@corp.ovh.com>
2018-02-08 16:22:59 +0100

    :tractor: refactor: remove vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,30 @@
 <template>
   <div id="app">
     <main-nav></main-nav>
-    <router-view/>
+    <main>
+      <home-section></home-section>
+      <about-section></about-section>
+      <works-section></works-section>
+      <contact-section></contact-section>
+    </main>
   </div>
 </template>
 
 <script>
 import MainNav from '@/components/MainNav/MainNav'
+import HomeSection from '@/components/HomeSection/HomeSection'
+import AboutSection from '@/components/AboutSection/AboutSection'
+import WorksSection from '@/components/WorksSection/WorksSection'
+import ContactSection from '@/components/ContactSection/ContactSection'
 
 export default {
   name: 'App',
   components: {
-    MainNav
+    MainNav,
+    HomeSection,
+    AboutSection,
+    WorksSection,
+    ContactSection
   }
 }
 </script>
