c9bc7e3b85547c15081f917eeff90671a232f193
Camille Gicquel <camille.gicquel@corp.ovh.com>
2018-02-12 13:31:07 +0100

    :lipstick: style(home): base styles

--- src/components/HomeSection/HomeSection.vue
+++ src/components/HomeSection/HomeSection.vue
@@ -1,5 +1,5 @@
 <template>
-<section class="main__section home">
+<section id="Home"  class="main__section home">
   <header class="home__header">
     <h1 class="home__ux-logo">
       <img src="@/assets/images/logos/logo_ux-team.svg" alt="OVH UX Team logo">
@@ -10,9 +10,9 @@
   </header>
   <section class="home__intro">
     <h2 class="home__intro-title">We are the UX Team of OVH</h2>
-    <div class="home__intro-text">
-      <p>OVH is a fast growing company specialized in web services. We build the interfaces for the OVH customers.</p>
-      <p>As web development enthusiasts, we love new technologies and challenges. In a technical context where things change quickly, we aim at creating innovative and effective interactions, while considering the technical legacy.</p>
+    <div>
+      <p class="home__intro-text">OVH is a fast growing company specialized in web services. We build the interfaces for the OVH customers.</p>
+      <p class="home__intro-text">As web development enthusiasts, we love new technologies and challenges. In a technical context where things change quickly, we aim at creating innovative and effective interactions, while considering the technical legacy.</p>
     </div>
   </section>
 </section>
@@ -24,5 +24,6 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+@import "./HomeSection";
 </style>
