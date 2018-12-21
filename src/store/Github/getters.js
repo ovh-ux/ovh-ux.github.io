5aaab720365d00a122371cefad864bebfc32e514
Steeve Vandecappelle <me@mizore.fr>
2018-12-21 10:37:33 +0100

    Initial commit - OVH/UX Website

--- src/store/Github/getters.js
+++
@@ -1,7 +0,0 @@
-export default {
-    mainNavItems: (state) => state.mainNavItems,
-    aboutRecentItems: (state) => state.aboutRecentItems,
-    aboutTechnologies: (state) => state.aboutTechnologies,
-    works: (state) => state.works,
-    contactMeans: (state) => state.contactMeans
-};
--- src/store/Github/mutation-types.js
+++
@@ -1,5 +0,0 @@
-export const NBR_REPOS = "NBR_REPOS";
-export const LAST_PUSHED = "LAST_PUSHED";
-export const ABOUT_TECHNO = "ABOUT_TECHNO";
-export const MANAGER = "MANAGER";
-export const OTHER_PROJECTS = "OTHER_PROJECTS";
